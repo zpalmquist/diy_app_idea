7d9fd1089bccca44712c3f6eb56d3a515047f71b
ZacharyLandes <zach.g.landes@gmail.com>
2018-09-03 17:13:44 -0400

    seeds

--- diy_app_api/db/seeds.rb
+++ diy_app_api/db/seeds.rb
@@ -6,78 +6,92 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 require 'csv'
-CSV.readlines('app/data/seed_data/administrative_areas.csv',{ headers: true,
-                                                              header_converters: :symbol }
-                                                              ) do |row|
-  AdministrativeArea.create!(
-    name: row[:name],
-    country_id: row[:country_id]
-  )
-end
+require 'pry'
 
-CSV.readlines('app/data/seed_data/band_events.csv',{ headers: true,
-  header_converters: :symbol }
-  ) do |row|
-    BandEvent.create!(
-      band_id: row[:band_id],
-      event_id: row[:event_id]
-    )
-  end
 
-  CSV.readlines('app/data/seed_data/bands.csv',{ headers: true,
-    header_converters: :symbol }
-    ) do |row|
-      Band.create!(
-        genre: row[:genre],
-        locality_id: row[:locality_id],
-        name: row[:name]
-      )
-    end
+Country.create!(name:"Turkey")
+AdministrativeArea.create!(name:"CO", country:Country.first)
+u = User.new(username:"tom",email:"tom@tom.com")
+u.save!(:validate => false)
+Locality.create(administrative_area:AdministrativeArea.first)
+Band.create(genre:"slitterstunk", name:"muzzlepits",locality:Locality.first )
+Venue.create(name:"clubadub", locality:Locality.first)
+Event.create(start_date:"4/5/208", end_date:"4/5/2018", start_time:"4/5/2018", end_time:"4/5/3028", venue: Venue.first)
 
-  CSV.readlines('app/data/seed_data/countries.csv',{ headers: true,
-    header_converters: :symbol }
-    ) do |row|
-      Country.create!(
-        name: row[:name]
-      )
-    end
 
-  CSV.readlines('app/data/seed_data/events.csv',{ headers: true,
-    header_converters: :symbol }
-    ) do |row|
-      Event.create!(
-        start_date: row[:start_date],
-        end_date: row[:end_date],
-        start_time: row[:start_time],
-        end_time: row[:end_time],
-        venue_id: row[:venue_id],
-        title:    row[:title]
-      )
-    end
+# CSV.foreach('app/data/seed_data/countries.csv', :headers => true) do |row|
+#   Country.create!(
+#     name: row["name"],
 
-  CSV.readlines('app/data/seed_data/localities.csv',{ headers: true,
-    header_converters: :symbol }
-    ) do |row|
-      Locality.create!(
-        name: row[:name],
-        administrative_area_id: row[:administrative_area_id]
-      )
-    end
+#   )
+# end
 
-  CSV.readlines('app/data/seed_data/venue_events.csv',{ headers: true,
-    header_converters: :symbol }
-    ) do |row|
-      VenueEvent.create!(
-        venue_id: row[:venue_id],
-        event_id: row[:event_id]
-      )
-    end
+# CSV.foreach('app/data/seed_data/administrative_areas.csv', :headers => true) do |row|
+#   AdministrativeArea.create!(
+#     name: row["name"],
+#     country_id: row["country_id"]
+#     )
+#     binding.pry
+# end
 
-  CSV.readlines('app/data/seed_data/venues.csv',{ headers: true,
-    header_converters: :symbol }
-    ) do |row|
-      Venue.create!(
-        name: row[:name],
-        locality_id: row[:locality_id]
-      )
-    end
+
+# CSV.foreach('app/data/seed_data/band_events.csv'
+#   ) do |row|
+#     BandEvent.create!(
+#       band_id: row[:band_id],
+#       event_id: row[:event_id]
+#     )
+#   end
+
+#   CSV.foreach('app/data/seed_data/bands.csv'
+#     ) do |row|
+#       Band.create!(
+#         genre: row[:genre],
+#         locality_id: row[:locality_id],
+#         name: row[:name]
+#       )
+#     end
+
+#   CSV.foreach('app/data/seed_data/countries.csv'
+#     ) do |row|
+#       Country.create!(
+#         name: row[:name]
+#       )
+#     end
+
+#   CSV.foreach('app/data/seed_data/events.csv'
+#     ) do |row|
+#       Event.create!(
+#         start_date: row[:start_date],
+#         end_date: row[:end_date],
+#         start_time: row[:start_time],
+#         end_time: row[:end_time],
+#         venue_id: row[:venue_id],
+#         title:    row[:title]
+#       )
+#     end
+
+#   CSV.foreach('app/data/seed_data/localities.csv'
+#     ) do |row|
+#       Locality.create!(
+#         name: row[:name],
+#         administrative_area_id: row[:administrative_area_id]
+#       )
+#     end
+
+#   CSV.foreach('app/data/seed_data/venue_events.csv'
+#     ) do |row|
+#       VenueEvent.create!(
+#         venue_id: row[:venue_id],
+#         event_id: row[:event_id]
+#       )
+#     end
+
+#   CSV.foreach('app/data/seed_data/venues.csv'
+#     ) do |row|
+#       Venue.create!(
+#         name: row[:name],
+#         locality_id: row[:locality_id]
+#       )
+#       puts 'Venue created'
+#     end
