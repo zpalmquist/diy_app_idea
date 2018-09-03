3127523ca5c79559742346af2f3446485118efd8
Zac Palmquist <zac.palmquist@gmail.com>
2018-09-03 14:21:59 -0600

    country model updates and tested updates

--- diy_app_api/spec/models/country_spec.rb
+++ diy_app_api/spec/models/country_spec.rb
@@ -1,7 +1,21 @@
 require 'rails_helper'
 
 describe Country, type: :model do
+  let(:country) { create(:country) }
   context "relationships" do
-  it {should have_many :administrative_areas}
+    it {should have_many :administrative_areas}
+  end
+  context "validations" do
+    it "is valid with valid attributes" do
+      expect(country).to be_valid
+    end
+    it "is not valid with no name" do
+      country.name = nil
+      expect(country).to_not be_valid
+    end
+    it "is not valid if country name already exists" do
+      duplicate_country = Country.new(name: country.name)
+      expect(duplicate_country).to_not be_valid
+    end
   end
 end
