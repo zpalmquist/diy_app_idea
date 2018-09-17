6f5a5e6447f7f69184749c5b82457db084343463
Zac Palmquist <zac.palmquist@gmail.com>
2018-09-16 21:44:35 -0600

    more devise setup

--- diy_app_api/app/controllers/devise/omniauth_callbacks_controller.rb
+++ diy_app_api/app/controllers/devise/omniauth_callbacks_controller.rb
@@ -1,15 +1,15 @@
-# class Devise::OmniauthCallbacksController < DeviseController
-#   def all
-#     user = User.create_user_from_omniauth(request.env["omniauth.auth"])
-#     if user.persisted?
-#       sign_in_and_redirect user, notice: "Signed In!"
-#     else
-#       session["devise.user.attributes"] = user.attributes
-#       redirect_to new_user_registration_url
-#     end
-#   end
-#
-#   alias_method :facebook, :all
-#   alias_method :google, :all
-#   alias_method :soundcloud, :all
-# end
+class Devise::OmniauthCallbacksController < DeviseController
+  def all
+    user = User.create_user_from_omniauth(request.env["omniauth.auth"])
+    if user.persisted?
+      sign_in_and_redirect user, notice: "Signed In!"
+    else
+      session["devise.user.attributes"] = user.attributes
+      redirect_to new_user_registration_url
+    end
+  end
+
+  alias_method :facebook, :all
+  alias_method :google, :all
+  alias_method :soundcloud, :all
+end
