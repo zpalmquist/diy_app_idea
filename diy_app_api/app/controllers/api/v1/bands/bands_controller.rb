44f76f8fa706909e1052f27db9150ac96fc00a7c
Zac Palmquist <zac.palmquist@gmail.com>
2018-12-21 16:48:53 -0600

    adding CRUD functionality for band

--- diy_app_api/app/controllers/api/v1/bands/bands_controller.rb
+++ diy_app_api/app/controllers/api/v1/bands/bands_controller.rb
@@ -8,8 +8,31 @@ class Api::V1::Bands::BandsController < ApplicationController
   end
 
   def create
+    band = Band.new(band_params)
+    band.save
+    if band.save!
+      redirect_to band_dashboard_path(band)
+    else
+      render json: { message: "Please try again" }
+    end
+  end
+
+  def update
+    band = Band.find_by(params[:id])
+    band.update(band_params)
+    if band.update!(band_params)
+      redirect_to band_path(band)
+    else
+      render :edit
+    end
   end
 
   def destroy
+  end
+
+  private
+
+  def band_params
+    params[:band].permit(:name, :locality)
   end
 end
