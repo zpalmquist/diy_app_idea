0a02a9829b37181a47555db360c3fff17766753c
Zac Palmquist <zac.palmquist@gmail.com>
2018-12-20 17:16:55 -0700

    tested routes, created helpers for new routes

--- diy_app_api/config/routes.rb
+++ diy_app_api/config/routes.rb
@@ -4,8 +4,8 @@ Rails.application.routes.draw do
 
   get '/api/v1/:username/dashboard', to: "api/v1/users/dashboard#index", as: :user_root
 
-  get '/api/v1/:username/bands_dashboard', to: "api/v1/bands/my_bands_dashboard#index"
-  get '/api/v1/:username/venues_dashboard', to: "api/v1/bands/my_venues_dashboard#index"
+  get '/api/v1/:username/bands_dashboard', to: "api/v1/bands/my_bands_dashboard#index", as: :all_bands_dashboard
+  get '/api/v1/:username/venues_dashboard', to: "api/v1/bands/my_venues_dashboard#index", as: :all_venues_dashboard
 
   scope module: :api, defaults: { format: :json }, path: 'api' do
     scope module: :v1, path: 'v1' do
