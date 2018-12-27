fc36802f4316ef46d91fdc6e1202916411b32d97
Zac Palmquist <zac.palmquist@gmail.com>
2018-12-27 10:08:18 -0600

    removes unnessicary tests

--- diy_app_api/spec/models/band_event_spec.rb
+++
@@ -1,8 +0,0 @@
-require 'rails_helper'
-
-describe BandEvent, type: :model do
-  context "relationships" do
-  it {should belong_to :band}
-  it {should belong_to :event}
-  end
-end
--- diy_app_api/spec/models/user_band_spec.rb
+++
@@ -1,8 +0,0 @@
-require 'rails_helper'
-
-describe UserBand, type: :model do
-  context "relationships" do
-    it {should belong_to :user}
-    it {should belong_to :band}
-  end
-end
